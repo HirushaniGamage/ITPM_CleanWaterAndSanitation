fffe76dae370ec955f11b6a2b80dec71340bfd16
HirushaniGamage <chanukshi@gmail.com>
2023-04-07 22:41:27 +0530

    gulley list implemented

--- lib/function/gulleyFunction.php
+++ lib/function/gulleyFunction.php
@@ -11,7 +11,7 @@ include_once('auto_id.php');
 
 class Gulley extends Main{
 
-      //lets create the Add request function
+      //lets create the Add Product Methord
 
 public function makerequest($name, $phone, $user, $address, $date, $remark){
 
@@ -22,7 +22,7 @@ public function makerequest($name, $phone, $user, $address, $date, $remark){
    //insert product to databace
  
   $sqlInsert2 = "INSERT INTO gulley_tbl VALUES('$productId','$name','$phone','$user','$address',
-  '$date','$remark',0,0,0,0,0);";
+  '$date','$remark',0,0,0,0,0,0);";
 
   //lets check the errors 
   if($this->dbResult->error){
@@ -40,9 +40,49 @@ public function makerequest($name, $phone, $user, $address, $date, $remark){
    
 }
 
+// this function use to getlist 
 
+public function gulleyList($id){
+
+  $sqlSelect = "SELECT * FROM gulley_tbl WHERE d_status = 0 ORDER BY id ASC;";
+   //lets check the errors 
+    if($this->dbResult->error){
+    echo($this->dbResult->error);
+    exit;
+   }
+ //sql execute 
+ $sqlResult = $this->dbResult->query($sqlSelect);
+
+  //check the number of rows
+  $nor = $sqlResult->num_rows;
+
+  if($nor > 0){
+    while($rec = $sqlResult->fetch_assoc()){
+      $status = "";
+      if($rec['admin'] == 0){ $status = '<span class="badge bg-warning">Warning for approval</span>';}
+      else if($rec['admin'] == 2){ $status = '<span class="badge bg-warning">Re-Date requesting</span>';}
+      else if($rec['done'] == 0){ $status = '<span class="badge bg-danger">Waiting for date</span>';}
+      else if($rec['done'] == 1){ $status = '<span class="badge bg-success">Completed</span>';}
+        echo('
+        <tr>
+          <th >'.$rec['id'].'</th>
+          <td>'.$rec['name'].'</td>
+          <td>'.$rec['date'].'</td>
+          <td>'.$rec['price'].'</td>
+          <td>'.$status.'</td>
+          <td><button type="button" onclick="edit(\''.$rec['id'].'\');" class="btn btn-warning">Edit</button> <button type="button" onclick="delete_pro(\''.$rec['id'].'\');" class="btn btn-danger">Delete</button></td>
+       </tr>
+              ');
+    }
+  }
+  else {echo('
+    <div class="alert alert-warning" role="alert">
+    No materials Are Found!
+  </div>');
+  }
 }
 
 
+}
 
 ?>
